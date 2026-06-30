0d67e79b580a7a686aa8d3f1b7882181cfc806bc
Atharv-bali <160755403+Atharv-bali@users.noreply.github.com>
2026-06-30 23:56:39 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int mini=prices[0],profit=0;
+        for(int i=0;i<n;i++) { //O(n)
+            profit = max(profit,prices[i]-mini);
+            mini = min(mini,prices[i]);
+        }
+        return profit;
+    }
+};
+//Time complexity: O(n)
+//Space complexity: O(n)
