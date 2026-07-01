20b7bff6986e4d0b4ce215a05589d58ae31caf66
Atharv-bali <160755403+Atharv-bali@users.noreply.github.com>
2026-07-01 09:01:50 +0530

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxi=0,n=s.size(),r=0,l=0;
+        unordered_map<char,int> mpp;
+        while(r<n) { //O(n)
+            mpp[s[r]]++;
+            while(mpp[s[r]]>1) { //O(1)
+                mpp[s[l]]--;
+                l++;
+            }
+            maxi = max(maxi,r-l+1);
+            r++;
+        }
+        return maxi;
+    }
+};
+//Time complexity: O(n)
+//Space complexity: O(n)
