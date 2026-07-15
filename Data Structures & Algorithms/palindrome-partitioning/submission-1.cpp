9e04827312f9f93a3dbbbdd8c01df1754689f05b
Atharv-bali <160755403+Atharv-bali@users.noreply.github.com>
2026-07-15 12:59:31 +0530

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool check(string s) {
+        int i=0,j=s.size()-1;
+        while(i<j) {
+            if(s[i]!=s[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    void solve(int index, string s, vector<string>& ans, vector<vector<string>>& res) {
+        if(index>=s.size()) {
+            res.push_back(ans);
+            return;
+        }
+        for(int i=1;i<=s.size()-index;i++) { //O(2^n)
+            if(check(s.substr(index,i))) {
+                ans.push_back(s.substr(index,i));
+                solve(index+i,s,ans,res);
+                ans.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> res;
+        vector<string> ans;
+        solve(0,s,ans,res);
+        return res;
+    }
+};
+//Time complexity: O(2^nlogn)
+//Space complexity: O(n)
