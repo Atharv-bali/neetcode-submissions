c60e6eb8c1f4c210c32d5fe3476e9e214812f455
Atharv-bali <160755403+Atharv-bali@users.noreply.github.com>
2026-09-08 08:51:31 +0530

    Add: first-missing-positive - submission-3

---
+++ Data Structures & Algorithms/first-missing-positive/submission-3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0;i<n;i++) {
+            if(nums[i]<0)
+                nums[i] = 0; 
+        }
+        for(int i=0;i<n;i++) {
+            int val = abs(nums[i]);
+            if(val>0 && val<=n) {
+                if(nums[val-1]>0)
+                    nums[val-1] = -1*nums[val-1];
+                else if(nums[val-1] == 0)
+                    nums[val-1] = -1*(n+2);
+            }
+        }
+        for(int i=0;i<n;i++) {
+            if(nums[i]>=0)
+                return i+1;
+        }
+        return n+1;
+    }
+};
+//Time complexity: O(n)
+//Space complexity: O(1)
